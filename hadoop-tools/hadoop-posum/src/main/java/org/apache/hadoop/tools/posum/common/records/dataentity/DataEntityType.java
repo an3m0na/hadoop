393137d75f344eecf54168a96737b9448095e0d1
Anemona Voinea <anemona.voinea@gmail.com>
2016-03-22 14:14:03 +0200

    DataMaster can send TaskProfile as well

--- hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/common/records/dataentity/DataEntityType.java
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/common/records/dataentity/DataEntityType.java
@@ -1,11 +1,8 @@
 package org.apache.hadoop.tools.posum.common.records.dataentity;
 
-import org.apache.hadoop.tools.posum.common.records.dataentity.AppProfile;
-import org.apache.hadoop.tools.posum.common.records.dataentity.HistoryProfile;
-import org.apache.hadoop.tools.posum.common.records.dataentity.JobProfile;
-import org.apache.hadoop.tools.posum.common.records.dataentity.TaskProfile;
 import org.apache.hadoop.tools.posum.common.records.dataentity.impl.pb.AppProfilePBImpl;
 import org.apache.hadoop.tools.posum.common.records.dataentity.impl.pb.JobProfilePBImpl;
+import org.apache.hadoop.tools.posum.common.records.dataentity.impl.pb.TaskProfilePBImpl;
 
 /**
  * Created by ane on 3/3/16.
@@ -13,7 +10,7 @@ import org.apache.hadoop.tools.posum.common.records.dataentity.impl.pb.JobProfil
 public enum DataEntityType {
     JOB("jobs", JobProfilePBImpl.class),
     JOB_HISTORY("jobs_history", JobProfile.class),
-    TASK("tasks", TaskProfile.class),
+    TASK("tasks", TaskProfilePBImpl.class),
     TASK_HISTORY("tasks_history", TaskProfile.class),
     APP("apps", AppProfilePBImpl.class),
     APP_HISTORY("apps_history", AppProfile.class),
