9d4bbad66d07733ae4e29ce0218888dd0de77436
Anemona Voinea <anemona.voinea@gmail.com>
2016-03-30 17:09:59 +0300

    posum master as a composite service

---
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/core/master/POSUMMasterContext.java
@@ -0,0 +1,29 @@
+package org.apache.hadoop.tools.posum.core.master;
+
+import org.apache.hadoop.tools.posum.common.DummyTokenSecretManager;
+import org.apache.hadoop.yarn.event.Dispatcher;
+
+/**
+ * Created by ane on 3/19/16.
+ */
+public class POSUMMasterContext {
+    private Dispatcher dispatcher;
+    private DummyTokenSecretManager tokenSecretManager;
+
+    public void setDispatcher(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+    public Dispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    public void setTokenSecretManager(DummyTokenSecretManager tokenSecretManager) {
+        this.tokenSecretManager = tokenSecretManager;
+    }
+
+    public DummyTokenSecretManager getTokenSecretManager() {
+        return tokenSecretManager;
+    }
+
+}
---
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/core/master/POSUMMasterService.java
@@ -0,0 +1,67 @@
+package org.apache.hadoop.tools.posum.core.master;
+
+import org.apache.hadoop.ipc.Server;
+import org.apache.hadoop.service.AbstractService;
+import org.apache.hadoop.tools.posum.common.DummyTokenSecretManager;
+import org.apache.hadoop.tools.posum.common.POSUMConfiguration;
+import org.apache.hadoop.tools.posum.common.records.dataentity.GeneralDataEntity;
+import org.apache.hadoop.tools.posum.common.records.protocol.*;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.ipc.YarnRPC;
+import org.apache.hadoop.yarn.util.Records;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * Created by ane on 3/19/16.
+ */
+public class POSUMMasterService extends AbstractService implements POSUMMasterProtocol {
+
+    POSUMMasterContext pmContext;
+    private Server server;
+    private InetSocketAddress bindAddress;
+
+    /**
+     * Construct the service.
+     *
+     * @param context service pmContext
+     */
+    public POSUMMasterService(POSUMMasterContext context) {
+        super(POSUMMasterService.class.getName());
+        this.pmContext = context;
+    }
+
+    @Override
+    protected void serviceStart() throws Exception {
+        YarnRPC rpc = YarnRPC.create(getConfig());
+        InetSocketAddress masterServiceAddress = getConfig().getSocketAddr(
+                POSUMConfiguration.PM_BIND_ADDRESS,
+                POSUMConfiguration.PM_ADDRESS,
+                POSUMConfiguration.DEFAULT_PM_ADDRESS,
+                POSUMConfiguration.DEFAULT_PM_PORT);
+        pmContext.setTokenSecretManager(new DummyTokenSecretManager());
+        this.server =
+                rpc.getServer(DataMasterProtocol.class, this, masterServiceAddress,
+                        getConfig(), pmContext.getTokenSecretManager(),
+                        getConfig().getInt(POSUMConfiguration.PM_SERVICE_THREAD_COUNT,
+                                POSUMConfiguration.DEFAULT_PM_SERVICE_THREAD_COUNT));
+
+        this.server.start();
+        this.bindAddress = getConfig().updateConnectAddr(
+                POSUMConfiguration.PM_BIND_ADDRESS,
+                POSUMConfiguration.PM_ADDRESS,
+                POSUMConfiguration.DEFAULT_PM_ADDRESS,
+                server.getListenerAddress());
+        super.serviceStart();
+    }
+
+    @Override
+    protected void serviceStop() throws Exception {
+        if (this.server != null) {
+            this.server.stop();
+        }
+        super.serviceStop();
+    }
+}
