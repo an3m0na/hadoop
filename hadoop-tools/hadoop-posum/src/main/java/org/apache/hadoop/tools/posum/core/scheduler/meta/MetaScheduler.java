ec012aa1f5b67432c9e7e37093d965372669aa90
Maria Voinea <M.Voinea@student.tudelft.nl>
2016-03-31 13:23:34 +0300

    dummy metascheduler

--- hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/core/scheduler/meta/MetaScheduler.java
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/core/scheduler/meta/MetaScheduler.java
@@ -1,7 +1,157 @@
 package org.apache.hadoop.tools.posum.core.scheduler.meta;
 
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.tools.posum.core.scheduler.basic.SQSAppAttempt;
+import org.apache.hadoop.tools.posum.core.scheduler.basic.SQSchedulerNode;
+import org.apache.hadoop.yarn.api.records.*;
+import org.apache.hadoop.yarn.server.resourcemanager.RMContext;
+import org.apache.hadoop.yarn.server.resourcemanager.recovery.RMStateStore;
+import org.apache.hadoop.yarn.server.resourcemanager.rmcontainer.RMContainer;
+import org.apache.hadoop.yarn.server.resourcemanager.rmcontainer.RMContainerEventType;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.AbstractYarnScheduler;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.Allocation;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.QueueMetrics;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.event.SchedulerEvent;
+import org.apache.hadoop.yarn.util.resource.ResourceCalculator;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by ane on 2/4/16.
  */
-public class MetaScheduler {
+public class MetaScheduler extends
+        AbstractYarnScheduler<SQSAppAttempt, SQSchedulerNode> implements
+        Configurable {
+
+    Configuration conf;
+
+    public MetaScheduler() {
+        super(MetaScheduler.class.getName());
+    }
+
+    @Override
+    public void serviceInit(Configuration conf) throws Exception {
+        //TODO initScheduler(conf);
+        super.serviceInit(conf);
+    }
+
+    @Override
+    public void serviceStart() throws Exception {
+        super.serviceStart();
+    }
+
+    @Override
+    public void serviceStop() throws Exception {
+        super.serviceStop();
+    }
+
+    @Override
+    public Configuration getConf() {
+        return conf;
+    }
+
+    @Override
+    public synchronized void setConf(Configuration conf) {
+        //TODO forward
+        this.conf = conf;
+    }
+
+    @Override
+    public int getNumClusterNodes() {
+        return nodes.size();
+    }
+
+    @Override
+    public synchronized void setRMContext(RMContext rmContext) {
+        this.rmContext = rmContext;
+    }
+
+    @Override
+    public synchronized void
+    reinitialize(Configuration conf, RMContext rmContext) throws IOException {
+        setConf(conf);
+    }
+
+    @Override
+    public Allocation allocate(
+            ApplicationAttemptId applicationAttemptId, List<ResourceRequest> ask,
+            List<ContainerId> release, List<String> blacklistAdditions, List<String> blacklistRemovals) {
+        //TODO forward
+        return null;
+    }
+
+    @Override
+    public void handle(SchedulerEvent event) {
+        //TODO forward
+    }
+
+    @Override
+    public QueueInfo getQueueInfo(String queueName,
+                                  boolean includeChildQueues, boolean recursive) {
+        return null; //TODO DEFAULT_QUEUE.getQueueInfo(false, false);
+    }
+
+    @Override
+    public List<QueueUserACLInfo> getQueueUserAclInfo() {
+        return null; //TODO DEFAULT_QUEUE.getQueueUserAclInfo(null);
+    }
+
+    @Override
+    public ResourceCalculator getResourceCalculator() {
+        return null; //TODO resourceCalculator;
+    }
+
+    @Override
+    public void recover(RMStateStore.RMState state) {
+        // NOT IMPLEMENTED
+    }
+
+    @Override
+    public RMContainer getRMContainer(ContainerId containerId) {
+        return null;
+        //TODO
+//        FiCaSchedulerApp attempt = getCurrentAttemptForContainer(containerId);
+//        return (attempt == null) ? null : attempt.getRMContainer(containerId);
+    }
+
+    @Override
+    protected void completedContainer(RMContainer rmContainer, ContainerStatus containerStatus, RMContainerEventType event) {
+       //TODO forward
+    }
+
+    @Override
+    public QueueMetrics getRootQueueMetrics() {
+        return null; //TODO  DEFAULT_QUEUE.getMetrics();
+    }
+
+    @Override
+    public synchronized boolean checkAccess(UserGroupInformation callerUGI,
+                                            QueueACL acl, String queueName) {
+        return false; //TODO DEFAULT_QUEUE.hasAccess(acl, callerUGI);
+    }
+
+    @Override
+    public synchronized List<ApplicationAttemptId> getAppsInQueue(String queueName) {
+        return null;
+        //TODO
+//        if (queueName.equals(DEFAULT_QUEUE.getQueueName())) {
+//            List<ApplicationAttemptId> attempts =
+//                    new ArrayList<ApplicationAttemptId>(applications.size());
+//            for (SchedulerApplication<FiCaSchedulerApp> app : applications.values()) {
+//                attempts.add(app.getCurrentAppAttempt().getApplicationAttemptId());
+//            }
+//            return attempts;
+//        } else {
+//            return null;
+//        }
+    }
+
+    public Resource getUsedResource() {
+        return null; //TODO usedResource;
+    }
+
+
 }
