de291787c9c1490f1ce64a91f9a42ca28615850c
Maria Voinea <M.Voinea@student.tudelft.nl>
2016-04-20 16:48:57 +0200

    dummy 3 second simulation

--- hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/simulator/master/Simulation.java
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/simulator/master/Simulation.java
@@ -1,15 +1,24 @@
 package org.apache.hadoop.tools.posum.simulator.master;
 
+import org.apache.hadoop.tools.posum.common.records.field.CompoundScore;
+import org.apache.hadoop.tools.posum.common.records.field.SimulationResult;
 import org.apache.hadoop.tools.posum.simulator.predictor.JobBehaviorPredictor;
 
 /**
  * Created by ane on 4/20/16.
  */
-public class Simulation extends Thread{
-    private boolean exit = false;
+public class Simulation extends Thread {
+    private volatile boolean exit = false;
+    private SimulatorImpl simulator;
+    private String policy;
+    private JobBehaviorPredictor predictor;
 
-    public Simulation(SimulatorImpl simulator, String policy, JobBehaviorPredictor predictor){
+
+    public Simulation(SimulatorImpl simulator, String policy, JobBehaviorPredictor predictor) {
         //TODO set up simulation
+        this.simulator = simulator;
+        this.policy = policy;
+        this.predictor = predictor;
     }
 
     void exit() {
@@ -19,8 +28,13 @@ public class Simulation extends Thread{
 
     @Override
     public void run() {
-        while (!exit) {
-            //TODO
+        //TODO actual code
+        try {
+            sleep(3000);
+            simulator.simulationDone(SimulationResult.newInstance(policy,
+                    CompoundScore.newInstance(Math.random() * 10, null, null)));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
