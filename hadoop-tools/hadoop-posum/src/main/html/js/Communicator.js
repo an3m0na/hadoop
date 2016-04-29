833792ccbf0b806d895b85686538ceb68c630549
Maria Voinea <M.Voinea@student.tudelft.nl>
2016-04-29 11:41:10 +0200

    posum web resources

---
+++ hadoop-tools/hadoop-posum/src/main/html/js/Communicator.js
@@ -0,0 +1,141 @@
+function Communicator(env) {
+    var self = this;
+    self.rootPath = "";
+    self.servicePath = self.rootPath + "/ajax";
+    var generalDialog = $("#general_dialog");
+    var loadingModal = $("#loading_modal");
+
+    self.showLoading = function(){
+        console.log("showing kitty");
+        loadingModal.modal("show");
+    };
+
+    self.hideLoading = function(){
+        console.log("hiding kitty");
+        loadingModal.modal("hide");
+    };
+
+    self.closeDialog = function () {
+        generalDialog.modal("hide");
+    };
+
+    self.showDialog = function (title, text, closeFunction, saveFunction) {
+        generalDialog.find(".an3-modal-title").text(title);
+        generalDialog.find(".an3-modal-text").text(text);
+        var closeButton = generalDialog.find(".an3-modal-close");
+        closeButton.off("click");
+        if (closeFunction)
+            closeButton.on("click", closeFunction);
+        var saveButton = generalDialog.find(".an3-modal-save");
+        if (!saveFunction)
+            saveButton.hide();
+        else {
+            saveButton.show().off("click").on("click", saveFunction);
+        }
+        generalDialog.modal();
+    };
+
+    $["postJSON"] = function (url, data, callback) {
+        // shift arguments if data argument was omitted
+        if ($.isFunction(data)) {
+            callback = data;
+            data = undefined;
+        }
+
+        return $.ajax({
+            url: url,
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(data),
+            success: callback
+        });
+    };
+
+    self.handleServerResponse = function (response, success, fail) {
+        if (typeof response === "string") {
+            response = JSON.parse(response);
+        }
+        if (response.successful) {
+            console.log("Received data");
+            console.log(response.result);
+            console.log("---");
+            success(response.result);
+        }
+        else {
+            self.handleServerError(null, response, fail);
+        }
+    };
+
+    self.handleServerError = function (jqXHR, result, fail) {
+        console.log(result);
+        console.log("---");
+        var parsedResult = null;
+
+        if (typeof result === "string") {
+            try {
+                parsedResult = JSON.parse(result);
+            } catch (e) {
+                console.log("Error result is not a JSON.");
+            }
+        } else {
+            parsedResult = result;
+        }
+
+        var errorObject = {
+            isGeneral: jqXHR ? true : false,
+            code: jqXHR ? jqXHR.status : parsedResult.result.code,
+            message: parsedResult ? parsedResult.result.message : result
+        };
+
+        if (fail)
+            fail(errorObject);
+        else {
+            self.showDialog("Error", "Error occurred:\n" +
+                errorObject.message +
+                "\n\nPlease fix the error and try again.");
+        }
+    };
+
+    function generalRequest(isPost, isRaw, showData, path, data, success, fail) {
+        console.log("---");
+        console.log("Sending " + (isPost ? "POST" : "GET") + " request to " + path + "...");
+        if (isPost && showData)
+            console.log(data);
+        var fullPath = self.servicePath + path;
+        var ajaxMethod = isPost ? (isRaw ? $.post : $.postJSON) : (isRaw ? $.get : $.getJSON);
+        (isPost ? ajaxMethod(fullPath, data) : ajaxMethod(fullPath))
+            .success(function (response) {
+                if (isRaw && !isPost) {
+                    success(response);
+                    return;
+                }
+                self.handleServerResponse(response, success, fail);
+            })
+            .fail(function (jqXHR, textStatus) {
+                self.handleServerError(jqXHR, textStatus, fail);
+            });
+    }
+
+    self.requestData = function (path, success, fail) {
+        generalRequest(false, false, true, path, null, success, fail);
+    };
+
+    self.postData = function (path, data, success, fail) {
+        generalRequest(true, false, true, path, data, success, fail);
+    };
+
+    self.requestRawData = function (path, success, fail) {
+        generalRequest(false, true, false, path, null, success, fail);
+    };
+
+    self.postRawData = function (path, data, success, fail) {
+        generalRequest(true, true, false, path, data, success, fail);
+    };
+
+    self.initialize = function () {
+        return self;
+    }
+
+
+}
---
+++ hadoop-tools/hadoop-posum/src/main/html/js/posum.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+    var env = {state: "home"};
+    var comm = new Communicator(env).initialize();
+    var tabManager = new TabManager(env).initialize();
+    env.comm = comm;
+    env.tabManager = tabManager;
+
+    if (location.hash && location.hash.length > 1)
+        env.state = location.hash.substr(1);
+
+    $("#div_title").on("click", function () {
+        window.location = comm.rootPath;
+    });
+
+    tabManager.changeState(env.state);
+});
