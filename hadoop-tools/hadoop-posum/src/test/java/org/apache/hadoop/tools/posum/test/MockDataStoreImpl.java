6648c841ca0ed1639c2d712e24f835e3c39f3cf8
Anemona Voinea <anemona.voinea@gmail.com>
2016-07-28 17:27:06 +0200

    mock data store has locks and uses maps

--- hadoop-tools/hadoop-posum/src/test/java/org/apache/hadoop/tools/posum/test/MockDataStoreImpl.java
+++ hadoop-tools/hadoop-posum/src/test/java/org/apache/hadoop/tools/posum/test/MockDataStoreImpl.java
@@ -8,120 +8,159 @@ import org.apache.hadoop.tools.posum.database.client.DBImpl;
 import org.apache.hadoop.tools.posum.database.client.DBInterface;
 import org.bson.types.ObjectId;
 
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by ane on 2/10/16.
  */
 public class MockDataStoreImpl implements MockDataStore {
 
-    private Map<DataEntityDB, Map<DataEntityType, List<? extends GeneralDataEntity>>> storedEntities = new HashMap<>();
+    private Map<DataEntityDB, Map<DataEntityType, Map<String, ? extends GeneralDataEntity>>> storedEntities =
+            new ConcurrentHashMap<>();
+    private ConcurrentHashMap<Integer, ReentrantReadWriteLock> locks = new ConcurrentHashMap<>();
 
     public MockDataStoreImpl() {
         for (DataEntityDB.Type type : DataEntityDB.Type.values()) {
-            Map<DataEntityType, List<? extends GeneralDataEntity>> dbEntities = new HashMap<>();
+            Map<DataEntityType, Map<String, ? extends GeneralDataEntity>> dbEntities = new HashMap<>();
             for (DataEntityType dataEntityType : DataEntityType.values()) {
-                dbEntities.put(dataEntityType, new LinkedList<GeneralDataEntity>());
+                dbEntities.put(dataEntityType, new LinkedHashMap<String, GeneralDataEntity>());
+
             }
             storedEntities.put(DataEntityDB.newInstance(type), dbEntities);
+            locks.put(type.ordinal(), new ReentrantReadWriteLock());
         }
     }
 
-    private <T extends GeneralDataEntity> List<T> getTypedEntities(DataEntityDB db, DataEntityType collection) {
-        return (List<T>) storedEntities.get(db).get(collection);
+    private <T extends GeneralDataEntity> Map<String, T> getTypedEntities(DataEntityDB db, DataEntityType collection) {
+        return (Map<String, T>) storedEntities.get(db).get(collection);
     }
 
 
     @Override
     public <T extends GeneralDataEntity> T findById(DataEntityDB db, DataEntityType collection, String id) {
-        for (T entity : this.<T>getTypedEntities(db, collection)) {
-            if (entity.getId().equals(id))
-                return entity;
+        locks.get(db.getId()).readLock().lock();
+        try {
+            return this.<T>getTypedEntities(db, collection).get(id);
+        } finally {
+            locks.get(db.getId()).readLock().unlock();
         }
-        return null;
     }
 
     @Override
     public List<String> listIds(DataEntityDB db, DataEntityType collection, Map<String, Object> queryParams) {
-        List<? extends GeneralDataEntity> entities = find(db, collection, queryParams, 0, 0);
-        List<String> ret = new ArrayList<>(entities.size());
-        for (GeneralDataEntity entity : entities) {
-            ret.add(entity.getId());
+        locks.get(db.getId()).readLock().lock();
+        try {
+            return new ArrayList<>(findEntitiesByParams(this.getTypedEntities(db, collection), queryParams, 0, 0).keySet());
+        } finally {
+            locks.get(db.getId()).readLock().unlock();
         }
-        return ret;
     }
 
     @Override
     public <T extends GeneralDataEntity> List<T> find(DataEntityDB db, DataEntityType collection, Map<String, Object> queryParams, int offset, int limit) {
-        List<T> entities = findEntitiesByParams(this.<T>getTypedEntities(db, collection), queryParams);
-        if (offset != 0) {
-            int skipValue = offset > 0 ? offset : entities.size() + offset;
-            if (skipValue > 0)
-                for (int i = skipValue; i > 0; i--)
-                    entities.remove(0);
+        locks.get(db.getId()).readLock().lock();
+        try {
+            return new ArrayList<>(findEntitiesByParams(this.<T>getTypedEntities(db, collection), queryParams, offset, limit).values());
+        } finally {
+            locks.get(db.getId()).readLock().unlock();
         }
-        if (limit != 0)
-            for (int i = limit; i < entities.size(); i++)
-                entities.remove(i);
-        return entities;
     }
 
-    private static <T> List<T> findEntitiesByParams(List<T> entities,
-                                                    Map<String, Object> params) {
+
+    private static <T> Map<String, T> findEntitiesByParams(Map<String, T> entities,
+                                                           Map<String, Object> params, int offset, int limit) {
         if (entities.size() < 1)
-            return Collections.emptyList();
-        List<T> results = new LinkedList<>();
+            return Collections.emptyMap();
+        Map<String, T> tmpResults = new LinkedHashMap<>();
+        Map<String, T> results = new LinkedHashMap<>();
+        Class entityClass = entities.values().iterator().next().getClass();
         try {
             Map<String, Method> propertyReaders =
-                    Utils.getBeanPropertyReaders(entities.get(0).getClass(), params.keySet());
-            for (T entity : entities)
-                if(Utils.checkBeanPropertiesMatch(entity, propertyReaders, params))
-                    results.add(entity);
-        } catch (Exception e) {
-            throw new POSUMException("Exception occured during findByParams " + e);
+                    Utils.getBeanPropertyReaders(entityClass, params.keySet());
+            for (Map.Entry<String, T> entry : entities.entrySet()) {
+                if (Utils.checkBeanPropertiesMatch(entry.getValue(), propertyReaders, params))
+                    tmpResults.put(entry.getKey(), entry.getValue());
+            }
+            int skip = offset >= 0 ? offset : tmpResults.size() + offset;
+            int count = limit != 0 ? limit : tmpResults.size();
+            for (Iterator<Map.Entry<String, T>> iterator = tmpResults.entrySet().iterator(); iterator.hasNext(); ) {
+                Map.Entry<String, T> next = iterator.next();
+                if (skip-- <= 0)
+                    results.put(next.getKey(), next.getValue());
+                if (--count == 0)
+                    break;
+            }
+
+        } catch (IntrospectionException | InvocationTargetException | IllegalAccessException e) {
+            throw new POSUMException("Reflection error while accessing entity properties", e);
         }
         return results;
     }
 
     @Override
     public <T extends GeneralDataEntity> String store(DataEntityDB db, DataEntityType collection, T toInsert) {
-        if (toInsert.getId() == null) {
-            toInsert.setId(ObjectId.get().toHexString());
-        } else {
-            if (findById(db, collection, toInsert.getId()) != null) {
-                throw new POSUMException("Duplicate id " + toInsert.getId());
+        locks.get(db.getId()).writeLock().lock();
+        try {
+            if (toInsert.getId() == null) {
+                toInsert.setId(ObjectId.get().toHexString());
+            } else {
+                if (findById(db, collection, toInsert.getId()) != null) {
+                    throw new POSUMException("Duplicate id " + toInsert.getId());
+                }
             }
+            this.getTypedEntities(db, collection).put(toInsert.getId(), toInsert);
+            return toInsert.getId();
+        } finally {
+            locks.get(db.getId()).writeLock().unlock();
         }
-        this.getTypedEntities(db, collection).add(toInsert);
-        return toInsert.getId();
     }
 
     @Override
     public <T extends GeneralDataEntity> boolean updateOrStore(DataEntityDB db, DataEntityType collection, T toUpdate) {
-        if (toUpdate.getId() == null) {
+        locks.get(db.getId()).writeLock().lock();
+        try {
+            if (toUpdate.getId() == null) {
+                store(db, collection, toUpdate);
+                return true;
+            }
+            delete(db, collection, toUpdate.getId());
             store(db, collection, toUpdate);
+
             return true;
+        } finally {
+            locks.get(db.getId()).writeLock().unlock();
         }
-        delete(db, collection, toUpdate.getId());
-        store(db, collection, toUpdate);
-        return true;
     }
 
     @Override
     public void delete(DataEntityDB db, DataEntityType collection, String id) {
-        for (Iterator<GeneralDataEntity> iterator = getTypedEntities(db, collection).iterator(); iterator.hasNext(); ) {
-            if (iterator.next().getId().equals(id))
-                iterator.remove();
+        locks.get(db.getId()).writeLock().lock();
+        try {
+
+            getTypedEntities(db, collection).remove(id);
+        } finally {
+            locks.get(db.getId()).writeLock().unlock();
         }
     }
 
     @Override
     public void delete(DataEntityDB db, DataEntityType collection, Map<String, Object> queryParams) {
-        List<String> ids = listIds(db, collection, queryParams);
-        for (Iterator<GeneralDataEntity> iterator = getTypedEntities(db, collection).iterator(); iterator.hasNext(); ) {
-            if (ids.contains(iterator.next().getId()))
-                iterator.remove();
+        locks.get(db.getId()).writeLock().lock();
+        try {
+
+            List<String> ids = listIds(db, collection, queryParams);
+            Map<String, ?> entities = storedEntities.get(db).get(collection);
+            for (String id : ids) {
+                entities.remove(id);
+            }
+        } finally {
+            locks.get(db.getId()).writeLock().unlock();
         }
     }
 
@@ -143,13 +182,18 @@ public class MockDataStoreImpl implements MockDataStore {
 
     @Override
     public void saveFlexFields(DataEntityDB db, String jobId, Map<String, String> newFields, boolean forHistory) {
-        DataEntityType type = forHistory ? DataEntityType.JOB_HISTORY : DataEntityType.JOB;
-        JobProfile job = findById(db, type, jobId);
-        if (job == null)
-            throw new POSUMException("Could not find job to save flex-fields: " + jobId);
+        locks.get(db.getId()).writeLock().lock();
+        try {
+            DataEntityType type = forHistory ? DataEntityType.JOB_HISTORY : DataEntityType.JOB;
+            JobProfile job = findById(db, type, jobId);
+            if (job == null)
+                throw new POSUMException("Could not find job to save flex-fields: " + jobId);
 
-        job.getFlexFields().putAll(newFields);
-        updateOrStore(db, type, job);
+            job.getFlexFields().putAll(newFields);
+            updateOrStore(db, type, job);
+        } finally {
+            locks.get(db.getId()).writeLock().unlock();
+        }
     }
 
 
