ce0e08532bb9baedfdcdad66422108592eb8c4f8
Anemona Voinea <anemona.voinea@gmail.com>
2017-01-07 14:16:18 +0100

    structure of a simulation

--- hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/simulation/core/Simulation.java
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/simulation/core/Simulation.java
@@ -4,20 +4,28 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.tools.posum.client.data.DataStore;
 import org.apache.hadoop.tools.posum.client.data.Database;
+import org.apache.hadoop.tools.posum.common.records.call.FindByIdCall;
 import org.apache.hadoop.tools.posum.common.records.call.FindByQueryCall;
+import org.apache.hadoop.tools.posum.common.records.call.IdsByQueryCall;
+import org.apache.hadoop.tools.posum.common.records.call.query.QueryUtils;
 import org.apache.hadoop.tools.posum.common.records.dataentity.DataEntityCollection;
 import org.apache.hadoop.tools.posum.common.records.dataentity.DatabaseReference;
 import org.apache.hadoop.tools.posum.common.records.dataentity.JobProfile;
+import org.apache.hadoop.tools.posum.common.records.dataentity.TaskProfile;
 import org.apache.hadoop.tools.posum.common.records.payload.CompoundScorePayload;
 import org.apache.hadoop.tools.posum.common.records.payload.SimulationResultPayload;
 import org.apache.hadoop.tools.posum.simulation.master.SimulationMaster;
 import org.apache.hadoop.tools.posum.simulation.predictor.JobBehaviorPredictor;
 
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static org.apache.hadoop.tools.posum.simulation.core.SimulationEvent.Type.TASK_FINISHED;
 
 
-public class Simulation implements Callable<SimulationResultPayload> {
+class Simulation implements Callable<SimulationResultPayload> {
     private static final Log logger = LogFactory.getLog(SimulationMaster.class);
 
     private volatile boolean exit = false;
@@ -25,26 +33,37 @@ public class Simulation implements Callable<SimulationResultPayload> {
     private JobBehaviorPredictor predictor;
     private DataStore dataStore;
     private DatabaseReference dbReference;
+    private Database db;
     private SimulationStatistics stats;
     private static final FindByQueryCall GET_LATEST =
             FindByQueryCall.newInstance(DataEntityCollection.JOB, null, "lastUpdated", true, 0, 1);
+    private Queue<SimulationEvent> eventQueue;
+    private Long clusterTime = 0L;
+    private Integer pendingJobs = 0;
+    private Double runtime = 0.0;
+    private Double penalty = 0.0;
+    private Double cost = 0.0;
 
 
-    public Simulation(JobBehaviorPredictor predictor, String policy, DataStore dataStore) {
+    Simulation(JobBehaviorPredictor predictor, String policy, DataStore dataStore) {
         this.predictor = predictor;
         this.policy = policy;
         this.dataStore = dataStore;
         this.stats = new SimulationStatistics();
+        this.eventQueue = new ConcurrentLinkedQueue<>();
     }
 
     private void setUp() {
         dbReference = DatabaseReference.get(DatabaseReference.Type.SIMULATION, policy);
         dataStore.clearDatabase(dbReference);
         dataStore.copyDatabase(DatabaseReference.getSimulation(), dbReference);
-        Database db = Database.extractFrom(dataStore, dbReference);
+        db = Database.extractFrom(dataStore, dbReference);
         predictor.initialize(db);
         stats.setStartTimeCluster(getLastUpdated());
         stats.setStartTimePhysical(System.currentTimeMillis());
+        IdsByQueryCall getPendingJobs =
+                IdsByQueryCall.newInstance(DataEntityCollection.JOB, QueryUtils.is("finishTime", 0L));
+        pendingJobs = db.executeDatabaseCall(getPendingJobs).getEntries().size();
     }
 
     private void tearDown() {
@@ -65,9 +84,10 @@ public class Simulation implements Callable<SimulationResultPayload> {
     public SimulationResultPayload call() throws Exception {
         setUp();
         try {
-            Thread.sleep(5000);
-            return SimulationResultPayload.newInstance(policy,
-                    CompoundScorePayload.newInstance(Math.random() * 10, 0.0, 0.0));
+            initializeDaemonSimulators();
+            predictBehavior();
+            processQueue();
+            return SimulationResultPayload.newInstance(policy, CompoundScorePayload.newInstance(runtime, penalty, cost));
         } catch (Exception e) {
             logger.error("Error during simulation. Shutting down simulation...", e);
             return SimulationResultPayload.newInstance(policy, CompoundScorePayload.newInstance(0.0, 0.0, 0.0));
@@ -75,4 +95,59 @@ public class Simulation implements Callable<SimulationResultPayload> {
             tearDown();
         }
     }
+
+    private void initializeDaemonSimulators() {
+        // TODO initialize the state of the hadoop daemon simulators (ResourceManager, ApplicationMasters and NodeManagers)
+    }
+
+    private void predictBehavior() {
+        IdsByQueryCall getAllTasks = IdsByQueryCall.newInstance(DataEntityCollection.TASK, null);
+        List<String> taskIds = dataStore.executeDatabaseCall(getAllTasks, DatabaseReference.getMain()).getEntries();
+        for (String taskId : taskIds) {
+            Long duration = predictor.predictTaskDuration(taskId);
+            eventQueue.add(new SimulationEvent<>(TASK_FINISHED, clusterTime + duration, new TaskFinishedDetails(taskId)));
+        }
+    }
+
+    private void processQueue() {
+        FindByIdCall getTask = FindByIdCall.newInstance(DataEntityCollection.TASK, null);
+        FindByIdCall getJob = FindByIdCall.newInstance(DataEntityCollection.JOB, null);
+        while (!exit) {
+            SimulationEvent event = eventQueue.poll();
+            clusterTime = event.getTimestamp();
+            switch (event.getType()) {
+                case TASK_FINISHED:
+                    getTask.setId(((TaskFinishedDetails) event.getDetails()).getTaskId());
+                    TaskProfile task = db.executeDatabaseCall(getTask).getEntity();
+                    task.setFinishTime(clusterTime);
+                    // update other task details
+
+                    JobProfile job = db.executeDatabaseCall(getJob).getEntity();
+                    if (checkLastTask(task, job)) {
+                        pendingJobs--;
+                        job.setFinishTime(clusterTime);
+                        // update other job details
+                        //TODO correctly update metrics
+                        runtime = Math.random() * 10;
+                        penalty++;
+                        cost++;
+                        if (pendingJobs == 0) {
+                            exit = true;
+                        }
+                    }
+                    // TODO update daemon simulators about task + job (job event is necessary?)
+                    break;
+            }
+        }
+    }
+
+    private boolean checkLastTask(TaskProfile task, JobProfile job) {
+        switch (task.getType()) {
+            case REDUCE:
+                return job.getTotalReduceTasks() - job.getCompletedReduces() == 1;
+            default:
+                return job.getTotalMapTasks() - job.getCompletedMaps() == 1;
+        }
+    }
+
 }
