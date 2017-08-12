ead0dd5bb9b9e8413d9053e589be0cd8be6578ed
anemona.voinea@gmail.com <maria.voinea@ing.nl>
2017-08-12 16:34:11 +0200

    rollback reasons are logged

--- hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/common/records/call/ThreePhaseDatabaseCallImpl.java
+++ hadoop-tools/hadoop-posum/src/main/java/org/apache/hadoop/tools/posum/common/records/call/ThreePhaseDatabaseCallImpl.java
@@ -1,10 +1,13 @@
 package org.apache.hadoop.tools.posum.common.records.call;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.tools.posum.common.records.dataentity.DatabaseReference;
 import org.apache.hadoop.tools.posum.common.records.payload.Payload;
 import org.apache.hadoop.tools.posum.data.core.LockBasedDataStore;
 
 abstract class ThreePhaseDatabaseCallImpl<T extends Payload> implements ThreePhaseDatabaseCall<T> {
+  private static Log logger = LogFactory.getLog(ThreePhaseDatabaseCallImpl.class);
 
   @Override
   public T executeCall(LockBasedDataStore dataStore, DatabaseReference db) {
@@ -14,6 +17,7 @@ abstract class ThreePhaseDatabaseCallImpl<T extends Payload> implements ThreePha
       commit(dataStore, db);
       return ret;
     } catch (Exception e) {
+      logger.debug("Exception occurred. Rolling back", e);
       rollBack(dataStore, db);
       throw e;
     }
