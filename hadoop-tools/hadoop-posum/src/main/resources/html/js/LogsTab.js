b4b181215d1aa6d76af233d3b9a7225043550e67
Anemona Voinea <anemona.voinea@gmail.com>
2017-10-29 16:42:20 +0100

    autoscroll btn

---
+++ hadoop-tools/hadoop-posum/src/main/resources/html/js/LogsTab.js
@@ -0,0 +1,39 @@
+function LogsTab(id, container, env) {
+  Tab.call(this, id, container, env);
+  var self = this;
+  self.autoScrollOn = true;
+  self.path = env.isTest ? "js/logs.json" : self.comm.dmPath + "/logs";
+  self.logTable = container.find("#log_table");
+  self.scrollBtn = container.find("#btn_scroll");
+  self.scrollBtn.bootstrapToggle().change(function () {
+    self.autoScrollOn = $(this).prop('checked');
+  });
+  self.activate = function () {
+
+    self.comm.requestData(self.path + "?since=" + self.lastRefreshed, function (data) {
+      if (!data || data.length === 0)
+        return;
+      data.forEach(function (log) {
+        const timestamp = moment.unix(log.timestamp / 1000);
+        const sameYear = moment().subtract(1, "years").isBefore(timestamp);
+        const sameDay = moment().subtract(1, "days").isBefore(timestamp);
+        const timeFormat = "HH:mm:ss";
+        const dateFormat = sameYear ? "MM-DD" : "YYYY-MM-DD";
+        const pattern = sameDay ? timeFormat : dateFormat + " " + timeFormat;
+        self.logTable.append('<tr class="info"><td class="text-nowrap">' +
+          timestamp.format(pattern) + '</td><td>' + log.message.replace(/\n/g, "<br/>") +
+          '</td></tr>');
+      });
+      if (self.autoScrollOn) {
+        self.scrollToBottom();
+      }
+      self.lastRefreshed = data[data.length - 1].timestamp;
+    });
+  };
+
+  self.scrollToBottom = function () {
+    $('body,html').stop().animate({scrollTop: self.logTable[0].scrollHeight}, 1500);
+  }
+}
+
+LogsTab.prototype = Object.create(Tab.prototype);
--- hadoop-tools/hadoop-posum/src/main/resources/html/js/posum.js
+++ hadoop-tools/hadoop-posum/src/main/resources/html/js/posum.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var env = {state: "home", isTest: false, refreshInterval: 2000, testTime: 0};
+  var env = {state: "home", isTest: true, refreshInterval: 2000, testTime: 0};
 
   if (location.hash && location.hash.length > 1)
     env.state = location.hash.substr(1);
@@ -8,9 +8,8 @@ $(document).ready(function () {
     window.location = "";
   });
 
-  var comm = new Communicator(env).initialize();
+  env.comm = new Communicator(env).initialize();
   var tabManager = new TabManager(env).initialize();
-  env.comm = comm;
   env.tabManager = tabManager;
 
   var path = env.isTest ? "js/conf.json" : env.comm.masterPath + "/conf";
