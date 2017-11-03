84c822e94e6bc4cb85a1dc3c94eeb22db715397c
Anemona Voinea <anemona.voinea@gmail.com>
2017-11-03 13:59:04 +0100

    system charts from DM

--- hadoop-tools/hadoop-posum/src/main/resources/html/js/SystemTab.js
+++ hadoop-tools/hadoop-posum/src/main/resources/html/js/SystemTab.js
@@ -1,175 +1,43 @@
 function SystemTab(id, container, env) {
   Tab.call(this, id, container, env);
   var self = this;
+  self.updateSystemCharts = function (componentAcronym, componentName, data) {
+    var plotPrefix = "plot_" + componentAcronym.toLowerCase();
+    var commonConfig = {
+      listExtractor: function (data) {
+        return data[componentAcronym];
+      },
+      baseTime: env.isTest ? env.testTime : 0
+    };
+    updateTimeSeriesPlot(self, plotPrefix + "_jvm", data, $.extend({}, commonConfig, {
+      entryValueExtractor: function (entry) {
+        return {Total: entry.jvm.total, Max: entry.jvm.max, Used: entry.jvm.used};
+      },
+      plotTitle: "JVM Memory for " + componentName,
+      yaxis: {title: "Memory (GB)"}
+    }));
+    updateTimeSeriesPlot(self, plotPrefix + "_cpu", data, $.extend({}, commonConfig, {
+      entryValueExtractor: function (entry) {
+        return {Total: entry.cpu.total, Process: entry.cpu.process};
+      },
+      plotTitle: "CPU Load for " + componentName,
+      yaxis: {title: "Fraction (%)"}
+    }));
+    updateTimeSeriesPlot(self, plotPrefix + "_threads", data, $.extend({}, commonConfig, {
+      entryValueExtractor: function (entry) {
+        return {Threads: entry.threadCount};
+      },
+      plotTitle: "Active Threads for " + componentName,
+      yaxis: {title: "Total Number"}
+    }));
+  };
   self.activate = function () {
-    var path = env.isTest ? "js/metrics_system.json" : self.comm.psPath + "/system";
+    var path = env.isTest ? "js/metrics_system.json" : self.comm.dmPath + "/all-systems";
     self.comm.requestData(path, function (data) {
-      updateTimeSeries(self,
-        "plot_ps_jvm",
-        data,
-        function (data) {
-          return data.jvm
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "JVM Memory for Portfolio Scheduler",
-        {title: "Memory (GB)", tickmode: "linear", dtick: 0.25}
-      );
-      updateTimeSeries(self,
-        "plot_ps_cpu",
-        data,
-        function (data) {
-          return data.cpu
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "CPU Load for Portfolio Scheduler",
-        {title: "Fraction (%)", tickmode: "auto", nticks: 10},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_ps_threads",
-        data,
-        function (data) {
-          return {"count": data.threadCount}
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "Active Threads for Portfolio Scheduler",
-        {title: "Total Number", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-    });
-
-    path = env.isTest ? "js/metrics_system.json" : self.comm.masterPath + "/system";
-    self.comm.requestData(path, function (data) {
-      updateTimeSeries(self,
-        "plot_pm_jvm",
-        data,
-        function (data) {
-          return data.jvm
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "JVM Memory for POSUM Master",
-        {title: "Memory (GB)", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_pm_cpu",
-        data,
-        function (data) {
-          return data.cpu
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "CPU Load for POSUM Master",
-        {title: "Fraction (%)", tickmode: "auto", nticks: 10},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_pm_threads",
-        data,
-        function (data) {
-          return {"count": data.threadCount}
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "Active Threads for POSUM Master",
-        {title: "Total Number", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-    });
-
-    path = env.isTest ? "js/metrics_system.json" : self.comm.dmPath + "/system";
-    self.comm.requestData(path, function (data) {
-      updateTimeSeries(self,
-        "plot_dm_jvm",
-        data,
-        function (data) {
-          return data.jvm
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "JVM Memory for Data Master",
-        {title: "Memory (GB)", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_dm_cpu",
-        data,
-        function (data) {
-          return data.cpu
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "CPU Load for Data Master",
-        {title: "Fraction (%)", tickmode: "auto", nticks: 10},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_dm_threads",
-        data,
-        function (data) {
-          return {"count": data.threadCount}
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "Active Threads for Data Master",
-        {title: "Total Number", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-    });
-    self.comm.requestData(path, function (data) {
-      path = env.isTest ? "js/metrics_system.json" : self.comm.smPath + "/system";
-      updateTimeSeries(self,
-        "plot_sm_jvm",
-        data,
-        function (data) {
-          return data.jvm
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "JVM Memory for Simulation Master",
-        {title: "Memory (GB)", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_sm_cpu",
-        data,
-        function (data) {
-          return data.cpu
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "CPU Load for Simulation Master",
-        {title: "Fraction (%)", tickmode: "auto", nticks: 10},
-        env.isTest ? env.testTime : 0
-      );
-      updateTimeSeries(self,
-        "plot_sm_threads",
-        data,
-        function (data) {
-          return {"count": data.threadCount}
-        },
-        function (traceObject) {
-          return traceObject
-        },
-        "Active Threads for Simulation Master",
-        {title: "Total Number", tickmode: "linear", dtick: 0.25},
-        env.isTest ? env.testTime : 0
-      );
+      self.updateSystemCharts("PS", "Portfolio Scheduler", data);
+      self.updateSystemCharts("OM", "Orchestration Master", data);
+      self.updateSystemCharts("DM", "Data Master", data);
+      self.updateSystemCharts("SM", "Simulation Master", data);
     });
   };
 }
