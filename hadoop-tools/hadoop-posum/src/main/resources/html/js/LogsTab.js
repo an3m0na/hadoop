47894cb25622c29671a423de14cf1c146e05a6ab
Anemona Voinea <anemona.voinea@gmail.com>
2018-01-14 12:45:28 +0100

    fix logs tab

--- hadoop-tools/hadoop-posum/src/main/resources/html/js/LogsTab.js
+++ hadoop-tools/hadoop-posum/src/main/resources/html/js/LogsTab.js
@@ -12,21 +12,21 @@ function LogsTab(id, container, env) {
     self.loading = true;
     var path = env.isTest ? "mocks/logs.json" : self.comm.paths.DM + "/logs";
     self.comm.requestData(path + "?since=" + self.lastRefreshed, function (data) {
-      if (!data || data.length === 0)
-        return;
-      data.forEach(function (log) {
-        const timestamp = moment.unix(log.timestamp / 1000);
-        const sameYear = moment().subtract(1, "years").isBefore(timestamp);
-        const sameDay = moment().subtract(1, "days").isBefore(timestamp);
-        const timeFormat = "HH:mm:ss";
-        const dateFormat = sameYear ? "MM-DD" : "YYYY-MM-DD";
-        const pattern = sameDay ? timeFormat : dateFormat + " " + timeFormat;
-        self.logTable.append('<tr class="info"><td class="text-nowrap">' +
-          timestamp.format(pattern) + '</td><td>' + log.message.replace(/\n/g, "<br/>") +
-          '</td></tr>');
-      });
-      if (self.autoScrollOn) {
-        self.scrollToBottom();
+      if (data) {
+        data.forEach(function (log) {
+          const timestamp = moment.unix(log.timestamp / 1000);
+          const sameYear = moment().subtract(1, "years").isBefore(timestamp);
+          const sameDay = moment().subtract(1, "days").isBefore(timestamp);
+          const timeFormat = "HH:mm:ss";
+          const dateFormat = sameYear ? "MM-DD" : "YYYY-MM-DD";
+          const pattern = sameDay ? timeFormat : dateFormat + " " + timeFormat;
+          self.logTable.append('<tr class="info"><td class="text-nowrap">' +
+            timestamp.format(pattern) + '</td><td>' + log.message.replace(/\n/g, "<br/>") +
+            '</td></tr>');
+        });
+        if (self.autoScrollOn) {
+          self.scrollToBottom();
+        }
       }
       self.lastRefreshed = data[data.length - 1].timestamp;
       self.loading = false;
